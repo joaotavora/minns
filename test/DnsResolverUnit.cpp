9422b8b0f35279dc176ac0408a0a270ac0065402
Joao Tavora <joaotavora@gmail.com>
2013-05-01 10:59:29 -0400

    cleanup: much simpler test makefiles and use googletest

--- test/DnsResolverUnit.cpp
+++ test/DnsResolverUnit.cpp
@@ -1,50 +1,19 @@
-// libc includes
-#include <stdlib.h>
-
-// stdl includes
+// libstdc++ includes
 #include <iostream>
 #include <fstream>
 #include <sstream>
 
 // project includes
 #include "DnsResolver.h"
+#include "gtest/gtest.h"
 
 // usings
 using namespace std;
 
 
-bool tryToResolveTest(int cachesize, int maxaliases, int maxinverseliases, const char* what){
-    try {
-        cout << "Starting tryToResolveTest() for " << what << " and (" <<
-        cachesize << ", " << maxaliases << ", " << maxinverseliases << ")" << endl;
-        DnsResolver resolver("../test/simplehosts.txt", cachesize, maxaliases, maxinverseliases);
-        stringstream ss(what);
-        string tosearch;
-        while (ss >> tosearch){
-            try {
-                string& result = resolver.resolve_to_string(tosearch);
-                cout << "  Resolved " << tosearch << " to " << result << endl;
-            } catch (DnsResolver::ResolveException& e) {
-                cout << "  Failed to resolve :" << e.what() << endl;
-            }
-            cout << "  " << resolver << endl;
-        }
-        return true;
-    } catch (std::exception& e) {
-        cout << "  Exception: " << e.what() << endl;
-        cout << "Failed: " << e.what() << endl;
-        return false;
-    }
+TEST(SucessfulResolution, SimpleNames) {
+
+DnsResolver resolver("test/simplehosts.txt", 10, 10, 2);
+EXPECT_NO_THROW(string result(resolver.resolve_to_string("bla")));
+EXPECT_EQ (1,1);
 }
-
-int main(int argc, char* argv[]){
-    cout << "Starting TcpSocket unit tests\n";
-    tryToResolveTest(10,10,2, "bla ble");
-    tryToResolveTest(2,10,2, "bla ble");
-    tryToResolveTest(2,10,2, "bla ble");
-    cout << "Done with TcpSocket unit tests\n";
-}
-
-
-
-
